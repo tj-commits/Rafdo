cdf46e5f5a79b5a2ce3bf5ff3fb20a96426a618d
tj-commits <thefoxjones@gmail.com>
2025-12-06 13:47:26 -0600

    fix

--- checkhttp.js
+++ checkhttp.js
@@ -1,1 +1,8 @@
-console.log("Html")
+console.log("67");;
+
+// Redirect to Netlify only if on rafdo.rf.gd
+if (window.location.hostname === 'rafdo.rf.gd') {
+  const currentPath = window.location.pathname + window.location.search + window.location.hash;
+  const targetUrl = 'https://rafdo.netlify.app' + currentPath;
+  window.location.replace(targetUrl);
+}
